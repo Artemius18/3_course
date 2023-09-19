d24011de5ddabde1bfce68dbc30cc6c07a6cd6c5
Artyom Pshenko <artempsenko@gmail.com>
2023-09-19 19:08:21 +0300

    Third OperatingSystem lab is done!

---
+++ 1term/OperatingSystems/Lab3/os03-01/OS03_03/OS03_03.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+
+int main() {
+    HANDLE hSnap;
+    PROCESSENTRY32 pe32;
+    system("chcp 1251>null");
+    // Создаем снимок всех процессов в системе
+    hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (hSnap == INVALID_HANDLE_VALUE) {
+        std::cerr << "Ошибка при создании снимка процессов: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // Устанавливаем размер структуры PROCESSENTRY32
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    // Получаем информацию о первом процессе
+    if (!Process32First(hSnap, &pe32)) {
+        std::cerr << "Ошибка при получении информации о первом процессе: " << GetLastError() << std::endl;
+        CloseHandle(hSnap);
+        return 1;
+    }
+
+    // Выводим информацию о каждом процессе
+    do {
+        std::cout << "ID процесса: " << pe32.th32ProcessID << std::endl;
+        std::wcout << L"Имя процесса: " << pe32.szExeFile << std::endl;
+    } while (Process32Next(hSnap, &pe32));
+
+    // Закрываем дескриптор снимка процессов
+    CloseHandle(hSnap);
+
+    return 0;
+}
