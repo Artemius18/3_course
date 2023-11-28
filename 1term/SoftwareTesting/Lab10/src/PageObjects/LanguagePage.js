26858f1a1b19343f94f19f0051814df3da49fcf3
Artyom <artempsenko@gmail.com>
2023-11-28 09:07:34 +0300

    i hope this version 3.0 will run successfull in jenkins

--- 1term/SoftwareTesting/Lab10/src/PageObjects/LanguagePage.js
+++ 1term/SoftwareTesting/Lab10/src/PageObjects/LanguagePage.js
@@ -6,8 +6,11 @@ class LanguagePage extends BasePage {
             await this.goToUrl('https://www.mts.by');
             const langButton = await this.findElementByXPath('/html/body/div[6]/header/div[1]/div/div/div[2]/div/div/a[2]');
             await langButton.click();
+            await this.driver.sleep(2000);
+            
             const englishButton = await this.findElementByXPath('/html/body/div[6]/header/div[2]/div/div/div[2]/div/button');
             const buttonText = await englishButton.getText();
+            console.log('Button Text:', buttonText);
             return buttonText === 'Sign In';
         } catch (error) {
             return false;
